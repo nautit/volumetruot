e845ef52e4026b86676fe445a03117aa6f283791
Kentech <nautit@gmail.com>
2017-09-16 17:06:37 +0700

    Sua lai het

--- VolumeTruot/VolumeTruot.Android/MainActivity.cs
+++ VolumeTruot/VolumeTruot.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace VolumeTruot.Droid
 {
-    [Activity(Label = "VolumeTruot", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "VolumeTruot", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation =ScreenOrientation.Landscape)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
--- VolumeTruot/VolumeTruot/MainPage.xaml.cs
+++ VolumeTruot/VolumeTruot/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,40 @@ namespace VolumeTruot
 {
     public partial class MainPage : ContentPage
     {
+        static double volume = 0;
+        static bool isChange = false;
         public MainPage()
         {
             InitializeComponent();
+        }
+        private void OnPanVolume(object sender, PanUpdatedEventArgs e)
+        {
+            
+            switch (e.StatusType)
+            {
+                
+                case GestureStatus.Started:
+                    isChange = true;
+                    Debug.WriteLine(isChange);
+                    break;
+                case GestureStatus.Running:
+                    Debug.WriteLine(e.TotalY);
+                    Debug.WriteLine(isChange);
+                    if (isChange && Math.Abs(e.TotalY) > 1){
+                        volume += e.TotalY;
+                        isChange = false;
+                        x_Volume.TranslateTo(0, volume, 100);
+                    }    
+                    break;
+                case GestureStatus.Completed:
+                    Debug.WriteLine("Muc volume: " + volume);
+                    break;
+            }
+                
+            
+            
+            
+            
         }
     }
 }
